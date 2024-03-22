79f58828a3f033076ee613ce5d86187404f6350c
trotsiuk <volodymyr.trotsiuk@wsl.ch>
2024-03-22 12:44:34 +0100

    changing the commit message

--- update_index.R
+++ update_index.R
@@ -3,20 +3,3 @@ quarto::quarto_render("index.qmd")
 system('git add .')
 system('git commit -a -m "Index update"')
 system('git push')
-
-# git2r::add(repo = ".", path = NULL, force = FALSE)
-repo_1 <- clone(path_bare, path_repo_1)
-config(repo_1, user.name = "Alice", user.email = "alice@example.org")
-
-
-
-
-git2r::commit(all=TRUE, message="index update")
-git2r::push()
-
-
-
-repo <- git2r::init(".")
-git2r::config(repo, user.name = "trotsiuk", user.email = "volodymyr.trotsiuk@wsl.ch")
-git2r::commit(repo, all=TRUE, message="index update")
-git2r::push(repo, "origin", "refs/heads/master")
